68daa42cdfa9dbb0d8e16b670ba914e1443ae7ce
绝情母蟑螂 <949590449@qq.com>
2026-03-04 16:06:19 +0800

    init frontend

---
+++ src/main/java/com/example/campusplatformbackend/CampusPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.campusplatformbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusPlatformBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusPlatformBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/campusplatformbackend/CampusPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campusplatformbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusPlatformBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
